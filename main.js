1326a573ceea8454bd0f022c1a4f6d93adc0ae60
wynnytran <wynny.me@gmail.com>
2020-10-04 14:43:21 +0300

    fix: nav styles change on scroll

--- main.js
+++ main.js
@@ -14,3 +14,24 @@ menuToggle.addEventListener('click', () => {
 		linksContainer.style.height = 0;
 	}
 });
+
+// Change Nav-style On-scroll
+const navBar = document.querySelector('.navbar');
+const hero = document.querySelector('.hero');
+const sectionOneOptions = {
+	rootMargin: '-500px 0px 0px 0px',
+};
+
+const sectionObserverOne = new IntersectionObserver(
+	(entries, sectionObserverOne) => {
+		entries.forEach((entry) => {
+			if (!entry.isIntersecting) {
+				navBar.classList.add('nav-scrolled');
+			} else {
+				navBar.classList.remove('nav-scrolled');
+			}
+		});
+	},
+	sectionOneOptions
+);
+sectionObserverOne.observe(hero);
